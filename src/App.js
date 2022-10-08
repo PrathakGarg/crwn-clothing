62198082a93c6ea3729552bfde3f1b80f9e964c0
Phoenix <prodotzip@gmail.com>
2022-10-09 04:44:56 +0530

    started navbar/better routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from "./routes/home/home.component";
+import Navbar from './routes/navbar/navbar.component';
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />} />
+      <Route path='/' element={<Navbar />} >
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,7 @@
+const Shop = () => {
+    return (
+        <div>This is me. Kirane waali dukaan</div>
+    )
+}
+
+export default Shop;
