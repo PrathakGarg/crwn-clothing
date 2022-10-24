3d301763fd5024f1461da4d1abef7b129a99b6a9
Phoenix <prodotzip@gmail.com>
2022-10-25 02:09:13 +0530

    Added and styled Product Cards

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,20 @@
 import { useContext } from "react";
 
+import ProductCard from "../../components/ProductCard/product-card.component";
+
 import { ProductsContext } from "../../contexts/products.context";
+
+import "./shop.styles.scss"
 
 const Shop = () => {
     const { products } = useContext(ProductsContext);
 
     return (
-        <div>
-            {products.map(({id, name}) => (
-                <div key={id}>
-                    <h1>{name}</h1>
-                </div>
+        <div className="products-container">
+            {products.map((product) => (
+                <ProductCard 
+                key={product.id}
+                product={product} />
             ))}
         </div>
     )
