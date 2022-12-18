47a2ec931bba7303ecd64d752a589efb2c5774ff
Phoenix <prodotzip@gmail.com>
2022-12-18 17:57:54 +0530

    QoL persistance changes

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,11 +1,27 @@
+import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
 
 import { userReducer } from "./user/user.reducer";
 import { categoriesReducer } from "./categories/categories.reducer";
 import { cartReducer } from "./cart/cart.reducer";
 
+const rootPersistConfig = {
+    key: "root",
+    storage,
+    blacklist: ["user", "cart"],
+};
+
+const cartPersistConfig = {
+    key: "cart",
+    storage,
+    blacklist: ["toggled"],
+}
+
 export const rootReducer = combineReducers({
     user: userReducer,
     categories: categoriesReducer,
-    cart: cartReducer,
+    cart: persistReducer(cartPersistConfig, cartReducer),
 })
+
+export const persistedRootReducer = persistReducer(rootPersistConfig, rootReducer);
--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,15 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from 'redux-persist/lib/storage'
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
-import { rootReducer } from "./root-reducer";
+import { persistedRootReducer } from "./root-reducer";
 
-const persistConfig = {
-    'key': 'root',
-    storage,
-    blacklist: ['user']
-}
+const middlewares = [
+  process.env.NODE_ENV === "development" && logger,
+].filter(Boolean);
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const composeEnhancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
-const middlewares = [process.env.NODE_ENV === "development" && logger].filter(Boolean)
-
-const composeEnhancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares))
-
-export const store = createStore(persistedReducer, undefined, composedEnhancers)
-export const persistor = persistStore(store)
+export const store = createStore(persistedRootReducer, undefined, composedEnhancers);
+export const persistor = persistStore(store);
