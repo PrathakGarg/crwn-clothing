23894bdb633b0a829a2b883417fb510f480fcd1a
PrathakGarg <prodotzip@gmail.com>
2023-01-03 03:03:51 +0530

    hooked serverless functions and integrated stripe API

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -10,17 +10,38 @@ const PaymentForm = () => {
 
     const paymentHandler = async (e) => {
         e.preventDefault();
-
-        if (!stripe || !elements) return;
         
+        if (!stripe || !elements) return;
+
+        const response = await fetch("/.netlify/functions/create-payment-intent", {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 10000 })
+        }).then(res => res.json())
+
+        const clientSecret = response.paymentIntent.client_secret
+        
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+          payment_method: {
+            card: elements.getElement(CardElement),
+            billing_details: {
+              name: "Prathak Garg",
+            },
+          }
+        })
+
+        if (paymentResult.error) console.log(paymentResult.error)
+        else if (paymentResult.paymentIntent.status === 'succeeded') alert('Payment Successful')
     }
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
-        <Button buttonType={BUTTON_CLASSES.inverted}>Pay Now</Button>
+        <Button buttonType={BUTTON_CLASSES.inverted}> Pay Now </Button>
       </FormContainer>
     </PaymentFormContainer>
   );
