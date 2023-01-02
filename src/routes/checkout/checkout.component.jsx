e1678b8b298b184fcf18c9cf8f3a21e96018c9dc
PrathakGarg <prodotzip@gmail.com>
2023-01-03 00:17:22 +0530

    added stripe payment in checkout (unconfigured)

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 
@@ -23,6 +24,7 @@ const Checkout = () => {
             {cartItems.map(product => <CheckoutItem key={product.id} product={product}/>)}
 
             <div className="total">Cart Total: ${cartTotal}</div>
+            <PaymentForm />
         </div>
     )
 }
