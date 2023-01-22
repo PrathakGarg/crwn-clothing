97e48ae6aadb91e52f9ea6dc13a85f25306f091f
PrathakGarg <prodotzip@gmail.com>
2023-01-23 01:12:24 +0530

    support wip temp done

--- src/routes/support/support.component.jsx
+++ src/routes/support/support.component.jsx
@@ -1,7 +1,19 @@
+import { SupportContentContainer } from "./support.styles";
+
 const Support = () => (
-    <div>
-        <h1>Support</h1>
-    </div>
-)
+  <>
+    <h1>Support</h1>
+    <SupportContentContainer>
+      <p>
+        In case of any concern to be raised, you may call at are 24/7 customer
+        support number +91-96496-68881 (Whatsapp)
+      </p>
+      <p>
+        We give our customer utmost priority and we will attempt to resolve any
+        grievences at the earliest
+      </p>
+    </SupportContentContainer>
+  </>
+);
 
 export default Support;
