ca6e4d5e176d8e0029909d6ab7fbe5b1bc8f373c
PrathakGarg <prathakgarg@gmail.com>
2023-04-17 01:21:04 +0530

    Used variables in gql endpoint. Replaced context

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client"
 
 import ProductCard from "../../components/ProductCard/product-card.component";
 import Spinner from "../../components/spinner/spinner.component";
@@ -8,28 +9,54 @@ import { CategoriesContext } from "../../contexts/categories.context";
 
 import "./category.styles.scss";
 
+const GET_CATEGORY = gql`
+  query ($title: String!){
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
+
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+
+  const { data, loading } = useQuery(GET_CATEGORY, {
+    variables: {title: category}
+  })
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [categoriesMap, category]);
+    if (data !== undefined) {
+      const { getCollectionsByTitle: { items } } = data
+      
+      setProducts(items)
+    }
+
+  }, [data, category]);
+  
+  // const { categoriesMap, loading } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
+
 
   return (
     <>
       {
         loading ? <Spinner /> : (
           <>
-          <h2 className="category-title">
+            <h2 className="category-title">
               <span>{category.toUpperCase()}</span>
-          </h2>
-          <div className="category-container">
-            {products && products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
+            </h2>
+            <div className="category-container">
+              {products && products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+            </div>
           </>
         )
       }
