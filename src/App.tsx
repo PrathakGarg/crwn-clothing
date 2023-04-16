39b6bcfb5858290964f71df2208e00875bfed54c
PrathakGarg <prathakgarg@gmail.com>
2023-04-17 02:43:07 +0530

    Lazy loading routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
+import { useEffect, lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-import Home from "./routes/home/home.component";
-import Navbar from "./routes/navbar/navbar.component";
-import Shop from "./routes/shop/shop.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Checkout from "./routes/checkout/checkout.component";
-import Support from "./routes/support/support.component";
-import TOS from "./routes/tos/tos.component";
-import Privacy from "./routes/privacy/privacy.component";
+import Spinner from "./components/spinner/spinner.component";
 
 import { checkUserSession } from "./store/user/user.action";
 
-// import SignUp from './routes/sign-up/sign-up.component';
+const Home = lazy(() => import("./routes/home/home.component"));
+const Navbar = lazy(() => import("./routes/navbar/navbar.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Authentication = lazy(() => import("./routes/authentication/authentication.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
+const Support = lazy(() => import("./routes/support/support.component"));
+const TOS = lazy(() => import("./routes/tos/tos.component"));
+const Privacy = lazy(() => import("./routes/privacy/privacy.component"));
+// const SignUp = lazy(() => import('./routes/sign-up/sign-up.component'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -23,18 +24,20 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="support" element={<Support />} />
-        <Route path="terms-of-service" element={<TOS />} />
-        <Route path="privacy" element={<Privacy />} />
-        {/* <Route path='signup' element={<SignUp />} /> */}
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="support" element={<Support />} />
+          <Route path="terms-of-service" element={<TOS />} />
+          <Route path="privacy" element={<Privacy />} />
+          {/* <Route path='signup' element={<SignUp />} /> */}
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
