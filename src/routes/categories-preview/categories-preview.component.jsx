68561b15dec5281a6617ab124b0a9c96b16c5fef
PrathakGarg <prathakgarg@gmail.com>
2023-04-17 01:04:55 +0530

    Add loading spinner in categories preview

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,20 +1,25 @@
 import { useContext, Fragment } from "react";
 
 import CategoryPreview from "../../components/category-preview/category-preview.component";
+import Spinner from "../../components/spinner/spinner.component"
 
 import { CategoriesContext } from "../../contexts/categories.context";
 
 const CategoriesPreview = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+  const { categoriesMap, loading } = useContext(CategoriesContext);
 
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title]
-        return (
-            <CategoryPreview key={title} title={title} products={products} />
+      {
+        loading ? <Spinner /> : (
+          Object.keys(categoriesMap).map((title) => {
+            const products = categoriesMap[title]
+            return (
+              <CategoryPreview key={title} title={title} products={products} />
+            )
+          })
         )
-      })}
+      }
     </Fragment>
   );
 };
