f79db221132cd62a36eb16e4628ae0870dd402c9
PrathakGarg <prathakgarg@gmail.com>
2023-04-17 01:02:43 +0530

    Added spinner component

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,10 @@
+import { SpinnerOverlay, SpinnerContainer } from "./spinner.styles";
+
+const Spinner = () => (
+    <SpinnerOverlay>
+        <SpinnerContainer />
+        <center>Loading...</center>
+    </SpinnerOverlay>
+)
+
+export default Spinner
