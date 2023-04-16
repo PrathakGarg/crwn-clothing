50330f55ad999b258a46981ed03d39af4751a661
PrathakGarg <prathakgarg@gmail.com>
2023-04-17 01:07:31 +0530

    Add loading spinner in categories

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,6 +2,7 @@ import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import ProductCard from "../../components/ProductCard/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
 
 import { CategoriesContext } from "../../contexts/categories.context";
 
@@ -9,7 +10,7 @@ import "./category.styles.scss";
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const { categoriesMap, loading } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -18,14 +19,20 @@ const Category = () => {
 
   return (
     <>
-    <h2 className="category-title">
-        <span>{category.toUpperCase()}</span>
-    </h2>
-    <div className="category-container">
-      {products && products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+      {
+        loading ? <Spinner /> : (
+          <>
+          <h2 className="category-title">
+              <span>{category.toUpperCase()}</span>
+          </h2>
+          <div className="category-container">
+            {products && products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+          </>
+        )
+      }
     </>
   );
 };
